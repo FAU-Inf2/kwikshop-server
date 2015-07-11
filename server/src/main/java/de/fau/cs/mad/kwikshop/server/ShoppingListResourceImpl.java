f50bfdb7b21c7e1ca253e3872969df093155fba6
Andreas Grünwald <andreasgruenwald@live.de>
2015-07-11 18:32:07 +0200

    Added ListResource as base interface for both shopping list and recipe resources

--- server/src/main/java/de/fau/cs/mad/kwikshop/server/ShoppingListResourceImpl.java
+++ server/src/main/java/de/fau/cs/mad/kwikshop/server/ShoppingListResourceImpl.java
@@ -1,9 +1,12 @@
 package de.fau.cs.mad.kwikshop.server;
 
+import com.wordnik.swagger.annotations.ApiParam;
+import de.fau.cs.mad.kwikshop.common.Item;
 import de.fau.cs.mad.kwikshop.common.ShoppingList;
 import de.fau.cs.mad.kwikshop.common.rest.ShoppingListResource;
 
 
+import javax.ws.rs.PathParam;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,14 +22,36 @@ public class ShoppingListResourceImpl implements ShoppingListResource {
 
 
     @Override
-    public List<ShoppingList> getShoppingList() {
-        return Arrays.asList(dummyList);
+    public List<ShoppingList> getList() {
+        return null;
     }
 
     @Override
-    public ShoppingList getShoppingList(int id) {
-        return dummyList;
+    public ShoppingList getList(@PathParam("listId") int id) {
+        return null;
     }
 
+    @Override
+    public List<Item> getListItem(@PathParam("listId") int listId,
+                                  @PathParam("itemId") int itemId) {
+        return null;
+    }
 
+    @Override
+    public Item createItem(@ApiParam(value = "Item to create", required = true) Item newItem) {
+        return null;
+    }
+
+    @Override
+    public Item updateItem(@ApiParam(value = "id of the list the item belongs to", required = true) @PathParam("listId") long listId,
+                           @ApiParam(value = "id of the Item to update", required = true) @PathParam("itemId") long itemId,
+                           @ApiParam(value = "new details of the specified item", required = true) Item item) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(@ApiParam(value = "id of the list the item belongs to", required = true) @PathParam("listId") long listId,
+                           @ApiParam(value = "id of the Item to update", required = true) @PathParam("itemId") long itemId) {
+
+    }
 }
