f96189002803fc58538232820dd48a9ebc23f171
Adrian Kretschmer <adrian@kremoe.de>
2015-09-15 11:42:46 +0200

    Added test class and a dummy test that always succeeds

---
+++ server/src/test/java/de/fau/cs/mad/kwikshop/server/ItemGraphTest.java
@@ -0,0 +1,11 @@
+package de.fau.cs.mad.kwikshop.server;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ItemGraphTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
