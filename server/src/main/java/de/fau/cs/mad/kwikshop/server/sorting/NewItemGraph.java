4e0aebfec44e9662a6284b969557dba305a9c1b3
Adrian Kretschmer <adrian@kremoe.de>
2015-09-23 16:39:46 +0200

    Added methods that mainly work with vertices and edges and tried to make them thread safe

--- server/src/main/java/de/fau/cs/mad/kwikshop/server/sorting/NewItemGraph.java
+++ server/src/main/java/de/fau/cs/mad/kwikshop/server/sorting/NewItemGraph.java
@@ -1,10 +1,13 @@
 package de.fau.cs.mad.kwikshop.server.sorting;
 
 import java.lang.ref.SoftReference;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import de.fau.cs.mad.kwikshop.common.ArgumentNullException;
 import de.fau.cs.mad.kwikshop.common.sorting.BoughtItem;
 
 public class NewItemGraph {
@@ -22,6 +25,9 @@ public class NewItemGraph {
     }
 
     public static NewItemGraph getItemGraph(DAOHelper daoHelper, Supermarket supermarket) {
+        if(supermarket == null) {
+            throw new ArgumentNullException("supermarket");
+        }
         NewItemGraph itemGraph = null;
         synchronized (supermarket) {
             // make sure only one item graph per supermarket is created
@@ -74,6 +80,17 @@ public class NewItemGraph {
         return items;
     }
 
+    private Vertex getVertexForBoughtItem(BoughtItem item) {
+        synchronized (vertices) {
+            for (Vertex v : vertices) {
+                if (v.getBoughtItem().equals(item)) {
+                    return v;
+                }
+            }
+        }
+        return null;
+    }
+
     public Set<Edge> getEdges() {
         Set<Edge> edges = new HashSet<>(); // size is not known at the moment
         synchronized (vertices) {
@@ -82,5 +99,69 @@ public class NewItemGraph {
             }
         }
         return edges;
+    }
+
+    public List<BoughtItem> getParents(BoughtItem child) {
+        List<BoughtItem> parents = new ArrayList<>();
+        synchronized (vertices) {
+            // this is probably not slower than iterating over all vertices and all descendants of all vertices
+            for(Edge edge: daoHelper.getEdgesByTo(child, supermarket)) {
+                if(edge.getTo().equals(child) && edge.getDistance() == 0)
+                    parents.add(edge.getFrom());
+            }
+        }
+        return parents;
+    }
+
+    public List<BoughtItem> getChildren(BoughtItem parent) {
+        List<Edge> edges;
+        synchronized (vertices) {
+            Vertex vertex = getVertexForBoughtItem(parent);
+            if (vertex == null) {
+                return null; //parent not contained in this item graph
+            }
+            edges = vertex.getEdges();
+        }
+        List<BoughtItem> children = new ArrayList<>(edges.size());
+        for (Edge edge : edges) {
+            children.add(edge.getTo());
+        }
+        return children;
+
+    }
+
+    public List<BoughtItem> getSiblings(BoughtItem child) {
+        List<BoughtItem> siblings = new ArrayList<>();
+        synchronized (vertices) {
+            for (BoughtItem parent : getParents(child)) {
+                siblings.addAll(getChildren(parent));
+            }
+        }
+        /* Remove all occurrences of this child */
+        while(siblings.remove(child)) {}
+
+        return siblings;
+    }
+
+    public Set<Edge> getEdgesFrom(BoughtItem boughtItem) {
+        List<Edge> edges;
+        synchronized (vertices) {
+            Vertex vertex = getVertexForBoughtItem(boughtItem);
+            if (vertex == null) {
+                return null; // item not contained in this item graph
+            }
+            edges=vertex.getEdges();
+        }
+        return new HashSet<>(edges);
+    }
+
+    //checks if there is an edge from node from to node to
+    public boolean edgeFromToExists(BoughtItem from, BoughtItem to){
+        Set<Edge> edges = getEdgesFrom(from);
+        for(Edge edge : edges) {
+            if(edge.getTo().equals(to))
+                return true;
+        }
+        return false;
     }
 }
